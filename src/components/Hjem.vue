f8b24cb521e52cabd5baf455aafa02f5e776869a
Marianne Haugvaldstad <mahaugv@stud.ntnu.no>
2024-10-18 12:04:56 +0200

    Adds home screen for medium screen

--- src/components/Hjem.vue
+++ src/components/Hjem.vue
@@ -18,19 +18,18 @@ const register = () => {
 </script>
 
 <template>
-    <div class="flex items-center justify-between">
+    <div class="flex flex-col md:flex-row items-center justify-between">
         
         <!-- Information Section -->
-        <div class="text-l w-[50%]">
+        <div class="text-l w-[50%] md:w-[50%] mb-8 md:mb-0">
             <p>Bokskapene i rommene CU1-111, CU2-021 og EU1-110 i Realfagbygget. Hvilket rom skapet er registrert på står på skapet.</p>
             <p>Hvis du vil ha et skap må du gå til ett av disse rommene, sette på en hengelås og registrere deg her.</p>
             <p class="mt-8">NB! Husk å sjekke at skapet ditt faktisk står registrert som "opptatt" under skaplister etter registrering.</p>
         </div>
 
-
         <!-- Form Section -->
-        <div class="bg-pink-500 shadow-pink text-white px-8 pt-4 pb-8 rounded-[32px]">
-            <div class="text-dark font-poppins text-title-1 px-10 mb-6">Reserver her</div>
+        <div class="bg-pink-500 w-[460px] md:w-[460px] shadow-pink text-white px-8 pt-4 pb-8 rounded-[32px]">
+            <div class="text-dark font-poppins text-title-1 text-center mb-6">Reserver her</div>
             
             <form @submit.prevent="register" class="flex flex-col space-y-4">
                 
@@ -49,7 +48,6 @@ const register = () => {
                         class="bg-blue-25 text-gray-700 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-pink-700"
                         v-model="selected" 
                         required
-                        
                         style="width: 120px;"
                     >
                         <option value="" disabled selected class="text-gray-500">Velg rom</option>
